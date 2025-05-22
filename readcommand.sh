9de2fb28b2b300078e148b794b2a0937a87b62e0
bana surekha reddy <surekhareddybana@gmail.com>
2025-05-22 14:00:34 +0530

    hello

--- readcommand.sh
+++ readcommand.sh
@@ -2,6 +2,6 @@
 person1=$1
 person2=$2
 read $PIN
-echo"hello person1::hi"
-echo"whtashappen $person2"
-echo"enter your pin number"
+echo "hello person1::hi"
+echo "whtashappen $person2"
+echo "enter your pin number"
