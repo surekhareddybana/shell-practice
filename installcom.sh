567941e9c099b3e511fdfdc007cbabfc6a2b4417
bana surekha reddy <surekhareddybana@gmail.com>
2025-05-25 16:56:32 +0530

    hello

--- installcom.sh
+++ installcom.sh
@@ -5,3 +5,10 @@ if [ $USERID -ne 0 ]
 then 
 echo "error: please run with root user id"
 fi
+dnf install mysql -y
+if [ $? -eq 0 ]
+then
+echo "installing of mysql is successful"
+else 
+echo "installing of mysql is unsuccessful"
+fi
