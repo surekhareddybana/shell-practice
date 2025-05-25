66f387d1aa444dae0111fb8efef623af680967f5
bana surekha reddy <surekhareddybana@gmail.com>
2025-05-25 11:52:25 +0530

    hello

--- conditions.sh
+++ conditions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 number=$1
-if [number lt 1]{
+if [number lt 1]
 	echo "given number $number is -lt 10 "
 
-else "if given $ number is not -lt 10"
+else "if given $number is not -lt 10"
 fi
