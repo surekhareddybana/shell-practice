97735c9f69c1ac0a1d526fec1423e83baabb1a5f
bana surekha reddy <surekhareddybana@gmail.com>
2025-05-26 15:46:37 +0530

    hello

--- instaldoubt.sh
+++ instaldoubt.sh
@@ -5,7 +5,7 @@ then
 echo "error::dnf installed  with root user"
 else 
 echo "plz install dnf with root user"
-exit1
+exit 1
 fi
 dnf install mysql -y
 
@@ -14,5 +14,5 @@ then
 echo "run with dnf successfully"
 else
 echo "install dnf with root user and fail"
-exit1
+exit 1
 fi
