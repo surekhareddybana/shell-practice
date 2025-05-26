bb863ce791d48d706ca7b9d56b2835c97fea4d61
bana surekha reddy <surekhareddybana@gmail.com>
2025-05-26 13:38:22 +0530

    hello

--- instaldoubt.sh
+++ instaldoubt.sh
@@ -2,8 +2,11 @@
 userid=$(id -u )
 if [ $userid -eq 0 ]
 then
+dnf list installed mysql 
+if [ $? -eq 0 ]
+then
 dnf install mysql -y
 echo "run with dnf successfully"
 else
-echo " install dnf with root user"
+echo " install dnf with root user and fail"
 fi
