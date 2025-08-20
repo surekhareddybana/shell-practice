46ed4accac79d56e27eb2f2981331686d34d5691
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 22:55:27 +0530

    hello

---
+++ 12.colors.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ echo -e "\e[31m hello colours"
+ echo "no colours" #hear we are not giving colour code but it is still coming with red colour..
+ #so if u dont want to come with colour we have to close given below
+ echo -e "\e[31m hello colours \e[0m"
+ echo "no colours"
+ echo -e "\e[31m hello \e[0m"
+ echo "no"
--- colors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- echo -e "\e[31m hello colours"
- echo "no colours"
- echo -e "\e[31m hello \e[0m"
- echo "no"
