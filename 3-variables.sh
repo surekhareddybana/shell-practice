89a792abab3e1025cc3f3e130b1f9705d187cdb0
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 20:28:33 +0530

    hello

--- 3-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-person1=trump #there should be no space between before and after =
-person2=musk
-echo "$person1::hi $person2"
-echo "$person2::hello $person1"
-echo "$person1::how r u $person2"
-echo "$person2:: iam good $person1, how r u"
