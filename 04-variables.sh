9372dc6a7b305beaadbf1b5f81e4728a68c8ba4d
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 16:03:43 +0530

    hello

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-person1=trump #there should be no space between before and after =
-person2=musk
+person1=$1 #there should be no space between before and after =
+person2=$2
 echo "$person1::hi $person2"
 echo "$person2::hello $person1"
 echo "$person1::how r u $person2"
