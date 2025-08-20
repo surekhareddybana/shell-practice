447559b2b96f7060ee9dc1311a867171e00e3f37
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 20:35:19 +0530

    hello

--- 08.specialvar.sh
+++ 08.specialvar.sh
@@ -2,3 +2,10 @@
 
 echo "all variables passed to the script: $@"
 echo "number of variables: $#"
+echo "script name: $0"
+echo "present working directiory: $PWD"
+echo "home directiory: $HOME"
+echo "user running the script: $USER"
+echo "process instant id of the current script: $$"
+sleep 10 &
+echo "pid of last command in background: $!"
