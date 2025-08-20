a0ddf2ef740b000153af7258cec56c579bf1c9e0
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 16:31:32 +0530

    hello

---
+++ 07.arraydatatype.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+movies=("sur" "jee" "nav" "bhan" "ladd")
+echo "${movies[0]}"
+echo "2nd movie is::${movies[2]}"
+echo "all movie names are ::${MOVIES[@]}"
--- arraydatatype.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-names=("sur" "jee" "nav" "bhan" "ladd")
-echo "${names[0]}"
-echo "2nd name is::${names[2]}"
