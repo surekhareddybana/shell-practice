ad1a0a58a66ffa05738a4f0783f766b815a063e7
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 21:04:21 +0530

    hello

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -2,8 +2,8 @@
 number=$1
 if [ number -lt 10 ]
 then
-	echo "given number $number is -lt 10 "
+	echo "given number $number is lessthan 10 "
 
 else 
-echo "if given number  $number is not -lt 10"
+echo "if given number  $number is not lesthan 10"
 fi
