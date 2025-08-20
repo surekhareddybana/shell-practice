03a9f77c72446ddc38021ca19c117868b4aa653e
bana surekha reddy <surekhareddybana@gmail.com>
2025-08-20 11:57:14 +0530

    variables

--- 3-variables.sh
+++ 3-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 person1=trump
 person2=musk
-echo "$persion1::hi persion2"
-echo "$persion2::hello $person1"
+echo "$person1::hi person2"
+echo "$person2::hello $person1"
 echo "$person1::how r u $person2"
 echo "$person2:: iam good $person1, how r u"
