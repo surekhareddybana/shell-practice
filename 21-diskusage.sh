7cca7bdd508c749ca8dea5e21262f20ea984c2de
bana surekha reddy <surekhareddybana@gmail.com>
2025-11-05 17:13:51 +0530

    hello cron

--- 21-diskusage.sh
+++ 21-diskusage.sh
@@ -2,7 +2,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #in project it will be 75
 
-while IFS=  line
+while IFS= read -r line
 do
 USAGE=$(echo $line | awk '{print $6F}' |  cut -d "%" -f1)
 PARTITION=$(echo $line | awk '{print $6F}')
