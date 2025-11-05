04e5962e587b256eecb933839c913943c988d2d6
bana surekha reddy <surekhareddybana@gmail.com>
2025-11-05 21:34:37 +0530

    hello

--- 21-diskusage.sh
+++ 21-diskusage.sh
@@ -2,6 +2,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #in project it will be 75
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 while IFS= read  line
 do
@@ -9,9 +10,10 @@ USAGE=$(echo $line | awk '{print $6F}' |  cut -d "%" -f1)
 PARTITION=$(echo $line | awk '{print $7F}')
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then
-   MSG="high disk usage on $PARTITION: $USAGE"
+   MSG+="high disk usage on $PARTITION: $USAGE \n"
    fi
 #echo "$PARTITION: $USAGE" 
 
 done <<< "$DISK_USAGE"
-echo $MSG
+ #echo -e $MSG
+ sh mail.sh "Devops Team" "High Disk Usage" $IP $MSG "surekhareddybana@gmail.com" "Alert high disk usage"
