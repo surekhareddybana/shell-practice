14b1ab18693d2dc8ae4a6bea8de8d5df279ad736
bana surekha reddy <surekhareddybana@gmail.com>
2025-11-05 17:27:58 +0530

    hello cron

--- 21-diskusage.sh
+++ 21-diskusage.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #in project it will be 75
+MSG=""
 
 while IFS= read -r line
 do
 USAGE=$(echo $line | awk '{print $6F}' |  cut -d "%" -f1)
-PARTITION=$(echo $line | awk '{print $6F}')
+PARTITION=$(echo $line | awk '{print $7F}')
+if [ $USAGE -gt $DISK_THRESHOLD]
+then
+MSG="high disk usage on $PARTITION: $USAGE"
 echo "$PARTITION: $USAGE" 
 
 done <<< $DISK_USAGE
