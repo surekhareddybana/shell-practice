36bd962581f1188f70b221d301f5c90308a631ab
bana surekha reddy <surekhareddybana@gmail.com>
2025-11-05 21:43:38 +0530

    hello

--- mail.sh
+++ mail.sh
@@ -7,7 +7,7 @@ MSG=$4
 FINAL_MSG=$(printf '%s\n' "$MSG" | sed -e 's/[]\$*.^[]/\\&/g')
 TO_ADDRESS=$5
 SUBJECT=$6
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$MSG/g")
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$MSG/g"
 template.html)
 {
 echo "To: $TO_ADDRESS"
